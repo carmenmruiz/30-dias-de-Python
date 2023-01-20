1d61a21f616c425099af869243306da7423c43ad
Marcos <marcodelarosaaaa@gmail.com>
2023-01-20 10:37:10 +0100

    Added day 1, 2 and 3

---
+++ day_2/variables.py
@@ -0,0 +1,64 @@
+#'Day 2: 30 Days of python programming'
+first_name = 'Carmen'
+last_name = 'Ruiz'
+full_name = 'Carmen Maria ruiz lama'
+country = 'España'
+City = 'jerez de la Frontera'
+age = '16'
+year = '2022'
+is_married = 'no'
+is_true= 'yes'
+is_ligth_on = 'no'
+year,age = '2022','16'
+
+#level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(City))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_ligth_on))
+print(type(year,age))
+
+print(len(first_name))
+print(len(last_name))
+
+num_one=5
+num_two=4
+
+num_total=num_one+num_two
+print(num_total)
+
+num_diff=num_one-num_two
+print(num_diff)
+
+num_product=num_one * num_two
+print(num_product)
+
+num_divide=num_one/num_two
+print(num_divide)
+
+num_exp=num_one**num_two
+print(num_exp)
+
+floor_divide=num_one//num_two
+print(floor_divide)
+
+
+radius=30
+area_of_circle=3.14 * radius**2
+
+circumference_of_circle=2 * 3.14 * radius
+print(circumference_of_circle)
+
+r=input("¿cual es el radio")
+r2= int(r)
+area=3.14 * r2 ** 2
+print(area)
+
+help('keywords')
+
---
+++ day_3/operation.py
@@ -0,0 +1,56 @@
+age=16
+print (age)
+
+height=1.68
+print('Floating point number, height: ', 1.68)
+
+complex= 7 + 3j
+print('complex number: ', 7 + 3j)
+
+b = 20
+h = 10
+area = 0.5 * b * h
+print(area)
+
+side_a = 5
+side_b = 4
+side_c = 3 
+perimeter = side_a + side_b + side_c
+print(perimeter)
+
+width = 3
+length = 4
+area_rectangulo = width * length
+perimeter_rectangulo = 2 * (width+length)
+print (area_rectangulo , perimeter_rectangulo)
+
+r1 = 8
+r2 = 2
+pi = 3.14
+radius_circle = r1 * r2 * pi
+circle_circunference = 2 * pi * r1
+print (radius_circle , circle_circunference)
+
+X= 20
+Y= 2 * X -2
+slope= Y
+print(slope)
+
+y1 = 2
+y2 = 10
+x1 = 2
+x2 = 6
+m = y2 - y1 / x1 - x2 
+slope = m
+print (slope)
+euclidian_distance = (y2-y1 + x2-x1)
+print(euclidian_distance)
+
+y = m
+print(y = m)
+
+
+
+
+
+
