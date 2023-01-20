49d732e25430f2eef4246684b0b9369d9686ca73
Marcos <marcodelarosaaaa@gmail.com>
2023-01-20 10:24:57 +0100

    Dia 1

---
+++ Día 1/helloworld.py
@@ -0,0 +1,25 @@
+print(type('your name:Carmen'))
+print(type('your family name:Ruiz'))
+print(type('your country:España'))
+print(type('I am enjoying 30 days of python'))
+print(type('10'))
+print(type('9.8'))
+print(type('3.14'))
+print(type('4-4j'))
+print['Asabeneh','Python','Finland']
+print(type('your name:Carmen'))
+print(type('your family name:Ruiz'))
+print(type('your country:España'))
+
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type('Asabeneh?'))
+print(type([1,2,3]))
+print(type({'name:Asabeneh'}))
+print(type({9.8, 3.14, 2.7}))
+
+print(2 - 10)
+print(3 - 8)
+
+
