b7ee0c8bbc3d553879a9635a5b593b2fa34eab27
Marcos <marcodelarosaaaa@gmail.com>
2023-03-10 10:33:46 +0100

    hasta dia 8

---
+++ day_6/tuples.py
@@ -0,0 +1,24 @@
+empty_tuple=tuple()
+
+tuple('brothers')
+brotrhers=('cesar','paco','juan','raul')
+
+tuple('sisters')
+sisters=('maria','lola','marta','lucia')
+
+tuple('siblings')
+siblings= brotrhers + sisters
+print(siblings)
+
+len(siblings)
+
+tuple('parents')
+parents=('carmen','miguel')
+
+tuple('family')
+family= siblings + parents
+print(family)
+
+
+
+
---
+++ day_8/dictionaries.py
@@ -0,0 +1,8 @@
+dog=dict()
+print(dog)
+
+dog={ 'raza':'labrador',
+'nombre':'doggy',
+'color':'blanco',
+'patas':'cuatro' ,
+'edad':'siete años'}
